e239e1350c01493ad1670ceaca3acbe88f3e35b9
skoulouzis <skoulouzis@users.noreply.github.com>
2023-06-07 13:46:08 +0200

    csddfv-dev-user-name-at-domain-com update

--- csddfv-dev-user-name-at-domain-com/csddfv-dev-user-name-at-domain-com.R
+++ csddfv-dev-user-name-at-domain-com/csddfv-dev-user-name-at-domain-com.R
@@ -1,30 +1,36 @@
-setwd('/app') 
+setwd('/app')
 
 # retrieve input parameters
-library(optparse) 
-option_list = list( 
-	 make_option(c("--numbers"), action="store", default=NA, type='character', help="my description"),
-	 make_option(c("--id"), action="store", default=NA, type='character', help="my description")
+
+library(optparse)
+library(jsonlite)
+
+option_list = list
+
+option_list = list(
+make_option(c("--id"), action="store", default=NA, type='character', help="my description"),
+make_option(c("--numbers"), action="store", default=NA, type='character', help="my description"),
 )
 
-# set input parameters accordingly 
-opt = parse_args(OptionParser(option_list=option_list)) 
-library(jsonlite) 
-numbers = fromJSON(opt$numbers) 
-id = opt$id 
+# set input parameters accordingly
+opt = parse_args(OptionParser(option_list=option_list))
 
-# check if the fields are set 
-if(is.na(numbers)){ 
-   stop('the `numbers` parameter is not correctly set. See script usage (--help)') 
-}
-if(is.na(id)){ 
-   stop('the `id` parameter is not correctly set. See script usage (--help)') 
-}
 
-# source code 
+id = opt$id
+numbers <- gsub("'", "", numbers)
+numbers <- gsub("[", "[\"", numbers)
+numbers <- gsub("]", "\"]", numbers)
+numbers = fromJSON(opt$numbers)
+
+
+
+
+
 average <- mean(numbers)
 
-# capturing outputs 
-file <- file(paste0('/tmp/average_', id, '.json')) 
-writeLines(toJSON(average, auto_unbox=TRUE), file) 
-close(file) 
+
+
+# capturing outputs
+file <- file(paste0('/tmp/average_', id, '.json'))
+writeLines(toJSON(average, auto_unbox=TRUE), file)
+close(file)
