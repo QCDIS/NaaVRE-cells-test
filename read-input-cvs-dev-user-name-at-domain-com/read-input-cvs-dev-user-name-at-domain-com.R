379fa5b2deb2f36119bee279e6513a70767f8c0f
skoulouzis <skoulouzis@users.noreply.github.com>
2023-06-26 13:19:03 +0200

    read-input-cvs-dev-user-name-at-domain-com update

--- read-input-cvs-dev-user-name-at-domain-com/read-input-cvs-dev-user-name-at-domain-com.R
+++ read-input-cvs-dev-user-name-at-domain-com/read-input-cvs-dev-user-name-at-domain-com.R
@@ -27,18 +27,18 @@ param_login = opt$param_login
 param_password = opt$param_password
 
 
-conf_local_datain1 = 'input/Phytoplankton__Progetto_Strategico_2009_2012_Australia.csv'
-conf_local_datain2 = 'input/Phytoplankton__Progetto_Strategico_2009_2012_Australia.csv'
-conf_datain1 = 'traits/input/Phytoplankton__Progetto_Strategico_2009_2012_Australia.csv'
-conf_datain2 = 'traits/input/2_FILEinformativo_OPERATORE.csv'
 conf_density = 1
+conf_local_datain2 = 'input/Phytoplankton__Progetto_Strategico_2009_2012_Australia.csv'
+conf_local_datain1 = 'input/Phytoplankton__Progetto_Strategico_2009_2012_Australia.csv'
+conf_datain2 = 'traits/input/2_FILEinformativo_OPERATORE.csv'
+conf_datain1 = 'traits/input/Phytoplankton__Progetto_Strategico_2009_2012_Australia.csv'
 
 
-conf_local_datain1 = 'input/Phytoplankton__Progetto_Strategico_2009_2012_Australia.csv'
-conf_local_datain2 = 'input/Phytoplankton__Progetto_Strategico_2009_2012_Australia.csv'
-conf_datain1 = 'traits/input/Phytoplankton__Progetto_Strategico_2009_2012_Australia.csv'
-conf_datain2 = 'traits/input/2_FILEinformativo_OPERATORE.csv'
 conf_density = 1
+conf_local_datain2 = 'input/Phytoplankton__Progetto_Strategico_2009_2012_Australia.csv'
+conf_local_datain1 = 'input/Phytoplankton__Progetto_Strategico_2009_2012_Australia.csv'
+conf_datain2 = 'traits/input/2_FILEinformativo_OPERATORE.csv'
+conf_datain1 = 'traits/input/Phytoplankton__Progetto_Strategico_2009_2012_Australia.csv'
 
 install.packages("RCurl",repos = "http://cran.us.r-project.org")
 RCurl = ''
@@ -46,7 +46,6 @@ library(RCurl)
 
 
 UserPwd = ''
-
 auth <- basicTextGatherer()
 auth$UserPwd <- paste(param_login, param_password, sep = ":")
 
@@ -99,39 +98,39 @@ write.table(df.datain,paste(output_dfdatain,sep=''),row.names=FALSE,sep = ";",de
 
 
 # capturing outputs
-file <- file(paste0('/tmp/numberoftransects_', id, '.json'))
-writeLines(toJSON(numberoftransects, auto_unbox=TRUE), file)
-close(file)
-file <- file(paste0('/tmp/dilutionfactor_', id, '.json'))
-writeLines(toJSON(dilutionfactor, auto_unbox=TRUE), file)
-close(file)
-file <- file(paste0('/tmp/transectcounting_', id, '.json'))
-writeLines(toJSON(transectcounting, auto_unbox=TRUE), file)
-close(file)
-file <- file(paste0('/tmp/settlingvolume_', id, '.json'))
-writeLines(toJSON(settlingvolume, auto_unbox=TRUE), file)
+file <- file(paste0('/tmp/auth_', id, '.json'))
+writeLines(toJSON(auth, auto_unbox=TRUE), file)
 close(file)
 file <- file(paste0('/tmp/diameteroffieldofview_', id, '.json'))
 writeLines(toJSON(diameteroffieldofview, auto_unbox=TRUE), file)
 close(file)
-file <- file(paste0('/tmp/index_', id, '.json'))
-writeLines(toJSON(index, auto_unbox=TRUE), file)
-close(file)
-file <- file(paste0('/tmp/numberofcountedfields_', id, '.json'))
-writeLines(toJSON(numberofcountedfields, auto_unbox=TRUE), file)
-close(file)
-file <- file(paste0('/tmp/auth_', id, '.json'))
-writeLines(toJSON(auth, auto_unbox=TRUE), file)
+file <- file(paste0('/tmp/transectcounting_', id, '.json'))
+writeLines(toJSON(transectcounting, auto_unbox=TRUE), file)
 close(file)
 file <- file(paste0('/tmp/UserPwd_', id, '.json'))
 writeLines(toJSON(UserPwd, auto_unbox=TRUE), file)
 close(file)
-file <- file(paste0('/tmp/output_dfmerged_', id, '.json'))
-writeLines(toJSON(output_dfmerged, auto_unbox=TRUE), file)
+file <- file(paste0('/tmp/numberofcountedfields_', id, '.json'))
+writeLines(toJSON(numberofcountedfields, auto_unbox=TRUE), file)
+close(file)
+file <- file(paste0('/tmp/settlingvolume_', id, '.json'))
+writeLines(toJSON(settlingvolume, auto_unbox=TRUE), file)
+close(file)
+file <- file(paste0('/tmp/dilutionfactor_', id, '.json'))
+writeLines(toJSON(dilutionfactor, auto_unbox=TRUE), file)
+close(file)
+file <- file(paste0('/tmp/index_', id, '.json'))
+writeLines(toJSON(index, auto_unbox=TRUE), file)
 close(file)
 file <- file(paste0('/tmp/diameterofsedimentationchamber_', id, '.json'))
 writeLines(toJSON(diameterofsedimentationchamber, auto_unbox=TRUE), file)
 close(file)
 file <- file(paste0('/tmp/countingStrategy_', id, '.json'))
 writeLines(toJSON(countingStrategy, auto_unbox=TRUE), file)
+close(file)
+file <- file(paste0('/tmp/numberoftransects_', id, '.json'))
+writeLines(toJSON(numberoftransects, auto_unbox=TRUE), file)
+close(file)
+file <- file(paste0('/tmp/output_dfmerged_', id, '.json'))
+writeLines(toJSON(output_dfmerged, auto_unbox=TRUE), file)
 close(file)
