8624f3ddc181c8bc0e2917e62a80884318063f72
skoulouzis <skoulouzis@users.noreply.github.com>
2023-06-29 11:56:05 +0200

    csddfv-dev-user-name-at-domain-com update

--- csddfv-dev-user-name-at-domain-com/csddfv-dev-user-name-at-domain-com.R
+++ csddfv-dev-user-name-at-domain-com/csddfv-dev-user-name-at-domain-com.R
@@ -9,7 +9,10 @@ option_list = list
 
 option_list = list(
 make_option(c("--id"), action="store", default=NA, type='character', help="my description"),
+
 make_option(c("--numbers"), action="store", default=NA, type='character', help="my description")
+
+
 )
 
 # set input parameters accordingly
