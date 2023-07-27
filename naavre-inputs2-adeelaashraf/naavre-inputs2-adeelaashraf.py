e376918531000c12b6f442b0f6fb83e499bdcb85
skoulouzis <skoulouzis@users.noreply.github.com>
2023-07-27 20:50:20 +0200

    naavre-inputs2-adeelaashraf update

--- naavre-inputs2-adeelaashraf/naavre-inputs2-adeelaashraf.py
+++ naavre-inputs2-adeelaashraf/naavre-inputs2-adeelaashraf.py
@@ -26,6 +26,10 @@ filename = "/tmp/password_" + id + ".json"
 file_password = open(filename, "w")
 file_password.write(json.dumps(password))
 file_password.close()
+filename = "/tmp/username_" + id + ".json"
+file_username = open(filename, "w")
+file_username.write(json.dumps(username))
+file_username.close()
 filename = "/tmp/num_files_" + id + ".json"
 file_num_files = open(filename, "w")
 file_num_files.write(json.dumps(num_files))
@@ -34,15 +38,11 @@ filename = "/tmp/mode_" + id + ".json"
 file_mode = open(filename, "w")
 file_mode.write(json.dumps(mode))
 file_mode.close()
-filename = "/tmp/remote_file_path_" + id + ".json"
-file_remote_file_path = open(filename, "w")
-file_remote_file_path.write(json.dumps(remote_file_path))
-file_remote_file_path.close()
-filename = "/tmp/username_" + id + ".json"
-file_username = open(filename, "w")
-file_username.write(json.dumps(username))
-file_username.close()
 filename = "/tmp/hostname_" + id + ".json"
 file_hostname = open(filename, "w")
 file_hostname.write(json.dumps(hostname))
 file_hostname.close()
+filename = "/tmp/remote_file_path_" + id + ".json"
+file_remote_file_path = open(filename, "w")
+file_remote_file_path.write(json.dumps(remote_file_path))
+file_remote_file_path.close()
