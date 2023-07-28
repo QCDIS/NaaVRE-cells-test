e7e2507302763d97065fafc45021e6c7f1cf414e
skoulouzis <skoulouzis@users.noreply.github.com>
2023-07-28 09:28:13 +0200

    untitled-gpelouze creation

---
+++ untitled-gpelouze/untitled-gpelouze.py
@@ -0,0 +1,28 @@
+
+import argparse
+arg_parser = argparse.ArgumentParser()
+
+arg_parser.add_argument('--id', action='store', type=str, required=True, dest='id')
+
+
+
+args = arg_parser.parse_args()
+print(args)
+
+id = args.id
+
+
+
+
+a = 'value'
+b = 3
+
+import json
+filename = "/tmp/b_" + id + ".json"
+file_b = open(filename, "w")
+file_b.write(json.dumps(b))
+file_b.close()
+filename = "/tmp/a_" + id + ".json"
+file_a = open(filename, "w")
+file_a.write(json.dumps(a))
+file_a.close()
