19a2ccd4cd27398aafd290c9409dc928f2e8c7a6
skoulouzis <skoulouzis@users.noreply.github.com>
2023-10-17 13:28:02 +0200

    loop-dev-user-name-at-domain-com creation

---
+++ loop-dev-user-name-at-domain-com/loop-dev-user-name-at-domain-com.py
@@ -0,0 +1,23 @@
+
+import argparse
+arg_parser = argparse.ArgumentParser()
+
+arg_parser.add_argument('--id', action='store', type=str, required=True, dest='id')
+
+
+arg_parser.add_argument('--msg', action='store', type=str, required='True', dest='msg')
+
+
+args = arg_parser.parse_args()
+print(args)
+
+id = args.id
+
+msg = args.msg
+
+
+
+
+for i in msg:
+    print(i)
+
