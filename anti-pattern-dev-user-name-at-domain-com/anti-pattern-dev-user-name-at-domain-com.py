3e81319b96842cafe42ce588381fbad5c9d7b0d5
skoulouzis <skoulouzis@users.noreply.github.com>
2023-10-20 14:30:05 +0200

    anti-pattern-dev-user-name-at-domain-com update

--- anti-pattern-dev-user-name-at-domain-com/anti-pattern-dev-user-name-at-domain-com.py
+++ anti-pattern-dev-user-name-at-domain-com/anti-pattern-dev-user-name-at-domain-com.py
@@ -9,6 +9,8 @@ arg_parser.add_argument('--a', action='store', type=int, required='True', dest='
 
 arg_parser.add_argument('--b', action='store', type=int, required='True', dest='b')
 
+arg_parser.add_argument('--msg', action='store', type=str, required='True', dest='msg')
+
 
 args = arg_parser.parse_args()
 print(args)
@@ -17,8 +19,10 @@ id = args.id
 
 a = args.a
 b = args.b
+msg = args.msg
 
 
 
 some_list = range(a, b+1)
+msg+='a'
 
