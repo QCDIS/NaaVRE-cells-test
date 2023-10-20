bdb97974bbdaac24ddc02464203baa00e80621a4
skoulouzis <skoulouzis@users.noreply.github.com>
2023-10-20 14:26:30 +0200

    add1-dev-user-name-at-domain-com creation

---
+++ add1-dev-user-name-at-domain-com/add1-dev-user-name-at-domain-com.py
@@ -0,0 +1,27 @@
+
+import argparse
+arg_parser = argparse.ArgumentParser()
+
+arg_parser.add_argument('--id', action='store', type=str, required=True, dest='id')
+
+
+arg_parser.add_argument('--a', action='store', type=int, required='True', dest='a')
+
+
+args = arg_parser.parse_args()
+print(args)
+
+id = args.id
+
+a = args.a
+
+
+
+
+b = a + 1
+
+import json
+filename = "/tmp/b_" + id + ".json"
+file_b = open(filename, "w")
+file_b.write(json.dumps(b))
+file_b.close()
