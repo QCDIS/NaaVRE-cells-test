a5291377b2f4a67ddf51a5482dfe2ce857fae43d
skoulouzis <skoulouzis@users.noreply.github.com>
2023-10-24 14:20:36 +0200

    loop-file-lines-dev-user-name-at-domain-com creation

---
+++ loop-file-lines-dev-user-name-at-domain-com/loop-file-lines-dev-user-name-at-domain-com.py
@@ -0,0 +1,26 @@
+
+import argparse
+arg_parser = argparse.ArgumentParser()
+
+arg_parser.add_argument('--id', action='store', type=str, required=True, dest='id')
+
+
+arg_parser.add_argument('--lines', action='store', type=str, required=True, dest='lines')
+
+
+args = arg_parser.parse_args()
+print(args)
+
+id = args.id
+
+import json
+lines = json.loads(args.lines.replace('\'','').replace('[','["').replace(']','"]'))
+
+
+
+
+count = 0
+for l in lines:
+    count += 1
+    print("Line{}: {}".format(count, l.strip()))
+
