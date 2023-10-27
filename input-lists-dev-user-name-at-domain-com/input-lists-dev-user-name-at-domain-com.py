c89c90240e52432a616fc4d158e872721e82dc40
skoulouzis <skoulouzis@users.noreply.github.com>
2023-10-27 17:13:29 +0300

    input-lists-dev-user-name-at-domain-com update

--- input-lists-dev-user-name-at-domain-com/input-lists-dev-user-name-at-domain-com.py
+++ input-lists-dev-user-name-at-domain-com/input-lists-dev-user-name-at-domain-com.py
@@ -19,11 +19,11 @@ list_of_paths = ["/webdav/LAZ/targets_myname","/webdav/LAZ/targets_myname","/web
 list_of_ints = [1,2,35,6,65]
 
 import json
-filename = "/tmp/list_of_ints_" + id + ".json"
-file_list_of_ints = open(filename, "w")
-file_list_of_ints.write(json.dumps(list_of_ints))
-file_list_of_ints.close()
 filename = "/tmp/list_of_paths_" + id + ".json"
 file_list_of_paths = open(filename, "w")
 file_list_of_paths.write(json.dumps(list_of_paths))
 file_list_of_paths.close()
+filename = "/tmp/list_of_ints_" + id + ".json"
+file_list_of_ints = open(filename, "w")
+file_list_of_ints.write(json.dumps(list_of_ints))
+file_list_of_ints.close()
