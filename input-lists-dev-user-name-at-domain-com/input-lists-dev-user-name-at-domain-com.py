9c88dd90f115a75cd690e1cea7e09ad2564c4c2e
skoulouzis <skoulouzis@users.noreply.github.com>
2023-10-28 17:45:43 +0300

    input-lists-dev-user-name-at-domain-com update

--- input-lists-dev-user-name-at-domain-com/input-lists-dev-user-name-at-domain-com.py
+++ input-lists-dev-user-name-at-domain-com/input-lists-dev-user-name-at-domain-com.py
@@ -5,18 +5,23 @@ arg_parser = argparse.ArgumentParser()
 arg_parser.add_argument('--id', action='store', type=str, required=True, dest='id')
 
 
+arg_parser.add_argument('--msg', action='store', type=str, required='True', dest='msg')
+
 
 args = arg_parser.parse_args()
 print(args)
 
 id = args.id
 
+msg = json.loads(args.msg.replace("'", '' ))
 
 
 
 
 list_of_paths = ["/webdav/LAZ/targets_myname","/webdav/LAZ/targets_myname","/webdav/LAZ/targets_myname","/webdav/LAZ/targets_myname","/webdav/LAZ/targets_myname","/webdav/LAZ/targets_myname","/webdav/LAZ/targets_myname","/webdav/LAZ/targets_myname","/webdav/LAZ/targets_myname","/webdav/LAZ/targets_myname"]
 list_of_ints = [1,2,35,6,65]
+
+print(msg)
 
 import json
 filename = "/tmp/list_of_paths_" + id + ".json"
