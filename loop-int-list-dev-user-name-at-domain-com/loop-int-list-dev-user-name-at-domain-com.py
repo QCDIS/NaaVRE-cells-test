0dc96ca79d87c92b9130f0d79bfeee72c4cd205a
skoulouzis <skoulouzis@users.noreply.github.com>
2023-10-28 20:02:51 +0300

    loop-int-list-dev-user-name-at-domain-com update

--- loop-int-list-dev-user-name-at-domain-com/loop-int-list-dev-user-name-at-domain-com.py
+++ loop-int-list-dev-user-name-at-domain-com/loop-int-list-dev-user-name-at-domain-com.py
@@ -5,7 +5,7 @@ arg_parser = argparse.ArgumentParser()
 arg_parser.add_argument('--id', action='store', type=str, required=True, dest='id')
 
 
-arg_parser.add_argument('--list_of_ints', action='store', type=str, required=True, dest='list_of_ints')
+arg_parser.add_argument('--list_of_ints', action='store', type=int, required='True', dest='list_of_ints')
 
 
 args = arg_parser.parse_args()
@@ -14,7 +14,7 @@ print(args)
 id = args.id
 
 import json
-list_of_ints = json.loads(args.list_of_ints)
+list_of_ints = args.list_of_ints
 
 
 
@@ -23,3 +23,8 @@ for i in list_of_ints:
     a = i -1
     print(a)
 
+import json
+filename = "/tmp/a_" + id + ".json"
+file_a = open(filename, "w")
+file_a.write(json.dumps(a))
+file_a.close()
