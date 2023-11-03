6a5306694543f38b2adba460c8d726c698785c51
Gabriel Pelouze <9514702+gpelouze@users.noreply.github.com>
2023-11-03 14:54:47 +0100

    print-a-dev-user-name-at-domain-com update

--- print-a-dev-user-name-at-domain-com/print-a-dev-user-name-at-domain-com.py
+++ print-a-dev-user-name-at-domain-com/print-a-dev-user-name-at-domain-com.py
@@ -5,7 +5,17 @@ arg_parser = argparse.ArgumentParser()
 arg_parser.add_argument('--id', action='store', type=str, required=True, dest='id')
 
 
-arg_parser.add_argument('--a', action='store', type=str, required=True, dest='a')
+arg_parser.add_argument('--a', action='store', type=int, required='True', dest='a')
+
+arg_parser.add_argument('--b', action='store', type=str, required=True, dest='b')
+
+arg_parser.add_argument('--c', action='store', type=str, required='True', dest='c')
+
+arg_parser.add_argument('--d', action='store', type=float, required='True', dest='d')
+
+arg_parser.add_argument('--e', action='store', type=int, required='True', dest='e')
+
+arg_parser.add_argument('--f', action='store', type=int, required='True', dest='f')
 
 
 args = arg_parser.parse_args()
@@ -13,11 +23,17 @@ print(args)
 
 id = args.id
 
+a = args.a
 import json
-a = json.loads(args.a)
+b = json.loads(args.b)
+c = args.c
+d = args.d
+e = args.e
+f = args.f
 
 
 
-for v in a:
+print(a, c, d, e, f)
+for v in b:
     print(v)
 
