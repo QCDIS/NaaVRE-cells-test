9abfb081640b1272bb151cee678ea57b4dbaf9aa
skoulouzis <skoulouzis@users.noreply.github.com>
2023-12-01 16:33:11 +0100

    inputr-jupyterhub-user creation

---
+++ inputr-jupyterhub-user/inputr-jupyterhub-user.R
@@ -0,0 +1,36 @@
+setwd('/app')
+
+# retrieve input parameters
+
+library(optparse)
+library(jsonlite)
+
+option_list = list
+
+option_list = list(
+make_option(c("--id"), action="store", default=NA, type='character', help="my description")
+)
+
+# set input parameters accordingly
+opt = parse_args(OptionParser(option_list=option_list))
+
+
+id = opt$id
+
+
+
+
+
+
+a = 2
+
+b = a + 1
+
+msg = "Hello world!"
+
+
+
+# capturing outputs
+file <- file(paste0('/tmp/msg_', id, '.json'))
+writeLines(toJSON(msg, auto_unbox=TRUE), file)
+close(file)
