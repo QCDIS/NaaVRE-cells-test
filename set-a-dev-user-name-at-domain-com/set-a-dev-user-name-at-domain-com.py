9fc672d31b141d6f0f000ca9297c70037e232c49
Gabriel Pelouze <9514702+gpelouze@users.noreply.github.com>
2023-12-04 11:36:59 +0100

    set-a-dev-user-name-at-domain-com update

--- set-a-dev-user-name-at-domain-com/set-a-dev-user-name-at-domain-com.py
+++ set-a-dev-user-name-at-domain-com/set-a-dev-user-name-at-domain-com.py
@@ -14,10 +14,25 @@ id = args.id
 
 
 
-a = [1, 2, 3]
+a = 1
+b = [1, 2]
+c = 'string'
+d = 1.3
 
 import json
 filename = "/tmp/a_" + id + ".json"
 file_a = open(filename, "w")
 file_a.write(json.dumps(a))
 file_a.close()
+filename = "/tmp/b_" + id + ".json"
+file_b = open(filename, "w")
+file_b.write(json.dumps(b))
+file_b.close()
+filename = "/tmp/c_" + id + ".json"
+file_c = open(filename, "w")
+file_c.write(json.dumps(c))
+file_c.close()
+filename = "/tmp/d_" + id + ".json"
+file_d = open(filename, "w")
+file_d.write(json.dumps(d))
+file_d.close()
