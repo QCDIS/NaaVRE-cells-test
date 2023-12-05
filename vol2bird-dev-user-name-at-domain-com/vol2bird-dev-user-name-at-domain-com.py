e6e327bced514943dda0795977f47124aaffc5c6
skoulouzis <skoulouzis@users.noreply.github.com>
2023-12-05 20:52:13 +0100

    vol2bird-dev-user-name-at-domain-com update

--- vol2bird-dev-user-name-at-domain-com/vol2bird-dev-user-name-at-domain-com.py
+++ vol2bird-dev-user-name-at-domain-com/vol2bird-dev-user-name-at-domain-com.py
@@ -6,7 +6,7 @@ arg_parser = argparse.ArgumentParser()
 arg_parser.add_argument('--id', action='store', type=str, required=True, dest='id')
 
 
-arg_parser.add_argument('--a', action='store', type=int, required='True', dest='a')
+arg_parser.add_argument('--a', action='store', type=int, required=True, dest='a')
 
 
 args = arg_parser.parse_args()
@@ -22,7 +22,6 @@ print(a)
 cmd = "vol2bird --version"
 
 msg = os.system(cmd)  # returns the exit code in unix
-
 
 import json
 filename = "/tmp/msg_" + id + ".json"
