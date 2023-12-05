bbf6379909b654e4e5b86d2372dd1b515abd7e12
skoulouzis <skoulouzis@users.noreply.github.com>
2023-12-05 20:11:05 +0100

    process-float-dev-user-name-at-domain-com update

--- process-float-dev-user-name-at-domain-com/process-float-dev-user-name-at-domain-com.py
+++ process-float-dev-user-name-at-domain-com/process-float-dev-user-name-at-domain-com.py
@@ -5,7 +5,7 @@ arg_parser = argparse.ArgumentParser()
 arg_parser.add_argument('--id', action='store', type=str, required=True, dest='id')
 
 
-arg_parser.add_argument('--c', action='store', type=float, required='True', dest='c')
+arg_parser.add_argument('--c', action='store', type=float, required=True, dest='c')
 
 
 args = arg_parser.parse_args()
@@ -13,7 +13,7 @@ print(args)
 
 id = args.id
 
-c = args.c.replace("'", "")
+c = args.c.replace('"', '')
 
 
 
