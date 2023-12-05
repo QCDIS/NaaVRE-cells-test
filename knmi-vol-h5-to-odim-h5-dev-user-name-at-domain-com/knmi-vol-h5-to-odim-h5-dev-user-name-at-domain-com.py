3af3be90deea6b096bd477f5ae6ac0c0450c64ea
skoulouzis <skoulouzis@users.noreply.github.com>
2023-12-05 16:37:12 +0100

    knmi-vol-h5-to-odim-h5-dev-user-name-at-domain-com creation

---
+++ knmi-vol-h5-to-odim-h5-dev-user-name-at-domain-com/knmi-vol-h5-to-odim-h5-dev-user-name-at-domain-com.py
@@ -0,0 +1,27 @@
+import os
+
+import argparse
+arg_parser = argparse.ArgumentParser()
+
+arg_parser.add_argument('--id', action='store', type=str, required=True, dest='id')
+
+
+
+args = arg_parser.parse_args()
+print(args)
+
+id = args.id
+
+
+
+
+
+cmd = "KNMI_vol_h5_to_ODIM_h5 "
+
+msg = os.system(cmd)  # returns the exit code in unix
+
+import json
+filename = "/tmp/msg_" + id + ".json"
+file_msg = open(filename, "w")
+file_msg.write(json.dumps(msg))
+file_msg.close()
