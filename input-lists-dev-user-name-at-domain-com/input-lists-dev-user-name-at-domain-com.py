2e9275e41db661f2eba618ed8fbf59206310d2e6
skoulouzis <skoulouzis@users.noreply.github.com>
2023-12-05 20:26:33 +0100

    input-lists-dev-user-name-at-domain-com update

--- input-lists-dev-user-name-at-domain-com/input-lists-dev-user-name-at-domain-com.py
+++ input-lists-dev-user-name-at-domain-com/input-lists-dev-user-name-at-domain-com.py
@@ -5,18 +5,22 @@ arg_parser = argparse.ArgumentParser()
 arg_parser.add_argument('--id', action='store', type=str, required=True, dest='id')
 
 
+arg_parser.add_argument('--msg', action='store', type=int, required='True', dest='msg')
+
 
 args = arg_parser.parse_args()
 print(args)
 
 id = args.id
 
+msg = args.msg
 
 
 
 
 list_of_paths = ["/webdav/LAZ/targets_myname","/webdav/LAZ/targets_myname","/webdav/LAZ/targets_myname","/webdav/LAZ/targets_myname","/webdav/LAZ/targets_myname","/webdav/LAZ/targets_myname","/webdav/LAZ/targets_myname","/webdav/LAZ/targets_myname","/webdav/LAZ/targets_myname","/webdav/LAZ/targets_myname"]
 list_of_ints = [1,2,35,6,65]
+print(msg)
 
 import json
 filename = "/tmp/list_of_paths_" + id + ".json"
