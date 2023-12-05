16cfbb5d4cb0e0de2f4811709011b44ecc9a99bb
skoulouzis <skoulouzis@users.noreply.github.com>
2023-12-05 16:23:56 +0100

    print-msg-dev-user-name-at-domain-com update

--- print-msg-dev-user-name-at-domain-com/print-msg-dev-user-name-at-domain-com.py
+++ print-msg-dev-user-name-at-domain-com/print-msg-dev-user-name-at-domain-com.py
@@ -5,7 +5,7 @@ arg_parser = argparse.ArgumentParser()
 arg_parser.add_argument('--id', action='store', type=str, required=True, dest='id')
 
 
-arg_parser.add_argument('--msg', action='store', type=str, required='True', dest='msg')
+arg_parser.add_argument('--msg', action='store', type=int, required='True', dest='msg')
 
 
 args = arg_parser.parse_args()
@@ -17,5 +17,5 @@ msg = args.msg
 
 
 
-print('msg:', msg)
+print('msg:', str(msg))
 
