34e4913db9c90fada3c2f4c2a93b8f5174a495b5
skoulouzis <skoulouzis@users.noreply.github.com>
2023-12-05 21:49:48 +0100

    input-lists-dev-user-name-at-domain-com update

--- input-lists-dev-user-name-at-domain-com/input-lists-dev-user-name-at-domain-com.py
+++ input-lists-dev-user-name-at-domain-com/input-lists-dev-user-name-at-domain-com.py
@@ -5,7 +5,7 @@ arg_parser = argparse.ArgumentParser()
 arg_parser.add_argument('--id', action='store', type=str, required=True, dest='id')
 
 
-arg_parser.add_argument('--msg', action='store', type=int, required=True, dest='msg')
+arg_parser.add_argument('--msg', action='store', type=str, required=True, dest='msg')
 
 
 args = arg_parser.parse_args()
@@ -13,7 +13,7 @@ print(args)
 
 id = args.id
 
-msg = args.msg
+msg = args.msg.replace('"','')
 
 
 
