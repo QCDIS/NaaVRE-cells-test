b556cc69d6b57158445578d0bd7a05bf367bca02
skoulouzis <skoulouzis@users.noreply.github.com>
2023-12-06 11:55:50 +0100

    r-input-a-and-b-dev-user-name-at-domain-com creation

---
+++ r-input-a-and-b-dev-user-name-at-domain-com/r-input-a-and-b-dev-user-name-at-domain-com.R
@@ -0,0 +1,39 @@
+setwd('/app')
+
+# retrieve input parameters
+
+library(optparse)
+library(jsonlite)
+
+option_list = list
+
+option_list = list(
+
+make_option(c(--"id"), action="store", default=NA, type="character", help="my description")
+
+)
+
+# set input parameters accordingly
+opt = parse_args(OptionParser(option_list=option_list))
+
+
+id = opt$id.replace('"','')
+
+
+
+
+
+
+a = 1 
+
+b = 2
+
+
+
+# capturing outputs
+file <- file(paste0('/tmp/a_', id, '.json'))
+writeLines(toJSON(a, auto_unbox=TRUE), file)
+close(file)
+file <- file(paste0('/tmp/b_', id, '.json'))
+writeLines(toJSON(b, auto_unbox=TRUE), file)
+close(file)
