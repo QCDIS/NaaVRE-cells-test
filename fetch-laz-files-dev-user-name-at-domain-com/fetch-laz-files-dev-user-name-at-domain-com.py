16cc36943eb3d366e086d5e4403f63aa9511f93b
skoulouzis <skoulouzis@users.noreply.github.com>
2023-12-15 18:47:24 +0100

    fetch-laz-files-dev-user-name-at-domain-com update

--- fetch-laz-files-dev-user-name-at-domain-com/fetch-laz-files-dev-user-name-at-domain-com.py
+++ fetch-laz-files-dev-user-name-at-domain-com/fetch-laz-files-dev-user-name-at-domain-com.py
@@ -35,7 +35,3 @@ filename = "/tmp/laz_files_" + id + ".json"
 file_laz_files = open(filename, "w")
 file_laz_files.write(json.dumps(laz_files))
 file_laz_files.close()
-filename = "/tmp/file_name_" + id + ".json"
-file_file_name = open(filename, "w")
-file_file_name.write(json.dumps(file_name))
-file_file_name.close()
