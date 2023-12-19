de91228f45db52a0d5ebc43e4be1fda61f0c45f1
skoulouzis <skoulouzis@users.noreply.github.com>
2023-12-19 15:40:49 +0100

    read-file-lines-dev-user-name-at-domain-com update

--- read-file-lines-dev-user-name-at-domain-com/read-file-lines-dev-user-name-at-domain-com.py
+++ read-file-lines-dev-user-name-at-domain-com/read-file-lines-dev-user-name-at-domain-com.py
@@ -1,3 +1,7 @@
+from os.path import isfile
+from os.path import join
+from os import listdir
+import os
 
 import argparse
 arg_parser = argparse.ArgumentParser()
@@ -5,7 +9,7 @@ arg_parser = argparse.ArgumentParser()
 arg_parser.add_argument('--id', action='store', type=str, required=True, dest='id')
 
 
-arg_parser.add_argument('--file_path', action='store', type=str, required='True', dest='file_path')
+arg_parser.add_argument('--file_path', action='store', type=str, required=True, dest='file_path')
 
 
 args = arg_parser.parse_args()
@@ -13,10 +17,16 @@ print(args)
 
 id = args.id
 
-file_path = args.file_path
+file_path = args.file_path.replace('"','')
 
 
+conf_data_folder = os.path.join('/tmp','data')
 
+conf_data_folder = os.path.join('/tmp','data')
+
+onlyfiles = [f for f in listdir(conf_data_folder) if isfile(join(conf_data_folder, f))]
+
+print(onlyfiles)
 
 f = open(file_path, 'r')
 lines = f.readlines()
