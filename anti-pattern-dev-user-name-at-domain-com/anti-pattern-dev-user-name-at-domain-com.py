471a8e4d9adbeef48c9add9d69cf7f9d0407ba3a
skoulouzis <skoulouzis@users.noreply.github.com>
2023-12-19 15:57:25 +0100

    anti-pattern-dev-user-name-at-domain-com update

--- anti-pattern-dev-user-name-at-domain-com/anti-pattern-dev-user-name-at-domain-com.py
+++ anti-pattern-dev-user-name-at-domain-com/anti-pattern-dev-user-name-at-domain-com.py
@@ -5,9 +5,9 @@ arg_parser = argparse.ArgumentParser()
 arg_parser.add_argument('--id', action='store', type=str, required=True, dest='id')
 
 
-arg_parser.add_argument('--a', action='store', type=int, required='True', dest='a')
+arg_parser.add_argument('--a', action='store', type=int, required=True, dest='a')
 
-arg_parser.add_argument('--b', action='store', type=int, required='True', dest='b')
+arg_parser.add_argument('--count', action='store', type=int, required=True, dest='count')
 
 
 args = arg_parser.parse_args()
@@ -16,9 +16,16 @@ print(args)
 id = args.id
 
 a = args.a
-b = args.b
+count = args.count
 
 
 
-some_list = range(a, b+1)
+some_list = range(count, a+1)
 
+msg = '1'
+
+import json
+filename = "/tmp/msg_" + id + ".json"
+file_msg = open(filename, "w")
+file_msg.write(json.dumps(msg))
+file_msg.close()
