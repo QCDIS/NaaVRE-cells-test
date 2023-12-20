fe0efe114518b77aaf9058f05814fd4821ba87e2
Gabriel Pelouze <9514702+gpelouze@users.noreply.github.com>
2023-12-20 15:32:10 +0100

    print-a-conf-param-dev-user-name-at-domain-com update

--- print-a-conf-param-dev-user-name-at-domain-com/print-a-conf-param-dev-user-name-at-domain-com.py
+++ print-a-conf-param-dev-user-name-at-domain-com/print-a-conf-param-dev-user-name-at-domain-com.py
@@ -5,9 +5,9 @@ arg_parser = argparse.ArgumentParser()
 arg_parser.add_argument('--id', action='store', type=str, required=True, dest='id')
 
 
-arg_parser.add_argument('--a', action='store', type=int, required='True', dest='a')
+arg_parser.add_argument('--a', action='store', type=int, required=True, dest='a')
 
-arg_parser.add_argument('--param_a', action='store', type=int, required='True', dest='param_a')
+arg_parser.add_argument('--param_a', action='store', type=int, required=True, dest='param_a')
 
 args = arg_parser.parse_args()
 print(args)
