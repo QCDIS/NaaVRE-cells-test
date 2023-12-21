b35c90fc09cc1cee5859f7a204ec92478f269d4b
Gabriel Pelouze <9514702+gpelouze@users.noreply.github.com>
2023-12-21 13:02:48 +0100

    print-a-conf-param-dev-user-name-at-domain-com creation

---
+++ print-a-conf-param-dev-user-name-at-domain-com/print-a-conf-param-dev-user-name-at-domain-com.R
@@ -0,0 +1,32 @@
+setwd('/app')
+
+# retrieve input parameters
+
+library(optparse)
+library(jsonlite)
+
+
+option_list = list(
+
+make_option(c("--a"), action="store", default=NA, type="integer", help="my description"), 
+make_option(c("--id"), action="store", default=NA, type="character", help="my description"), 
+make_option(c("--param_a"), action="store", default=NA, type="character", help="my description")
+
+)
+
+# set input parameters accordingly
+opt = parse_args(OptionParser(option_list=option_list))
+
+
+a = opt$a
+id <- gsub('"', '', opt$id)
+
+param_a = opt$param_a
+
+
+
+
+print(a, param_a)
+
+
+
