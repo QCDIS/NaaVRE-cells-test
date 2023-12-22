6ed99a866d65838986aeeae630e0121e8a2a9e06
skoulouzis <skoulouzis@users.noreply.github.com>
2023-12-22 15:14:02 +0100

    vislulization-param-dev-user-name-at-domain-com update

--- vislulization-param-dev-user-name-at-domain-com/vislulization-param-dev-user-name-at-domain-com.py
+++ vislulization-param-dev-user-name-at-domain-com/vislulization-param-dev-user-name-at-domain-com.py
@@ -28,29 +28,30 @@ conf_remote_path_geotiffs = pathlib.Path('/webdav/vl-laserfarm/' +  'spiros.koul
 conf_num_files = 1
 conf_mode = 'webdav'
 hostname = param_hostname
-login = param_login
+username = param_login
 password = param_password
-remote_path_geotiffs = str(conf_remote_path_geotiffs)
+remote_file_path = str(conf_remote_path_geotiffs)
 num_files = conf_num_files
 mode = conf_mode
+output_dir = '/tmp/data'
 
 import json
 filename = "/tmp/hostname_" + id + ".json"
 file_hostname = open(filename, "w")
 file_hostname.write(json.dumps(hostname))
 file_hostname.close()
-filename = "/tmp/login_" + id + ".json"
-file_login = open(filename, "w")
-file_login.write(json.dumps(login))
-file_login.close()
+filename = "/tmp/username_" + id + ".json"
+file_username = open(filename, "w")
+file_username.write(json.dumps(username))
+file_username.close()
 filename = "/tmp/password_" + id + ".json"
 file_password = open(filename, "w")
 file_password.write(json.dumps(password))
 file_password.close()
-filename = "/tmp/remote_path_geotiffs_" + id + ".json"
-file_remote_path_geotiffs = open(filename, "w")
-file_remote_path_geotiffs.write(json.dumps(remote_path_geotiffs))
-file_remote_path_geotiffs.close()
+filename = "/tmp/remote_file_path_" + id + ".json"
+file_remote_file_path = open(filename, "w")
+file_remote_file_path.write(json.dumps(remote_file_path))
+file_remote_file_path.close()
 filename = "/tmp/num_files_" + id + ".json"
 file_num_files = open(filename, "w")
 file_num_files.write(json.dumps(num_files))
@@ -59,3 +60,7 @@ filename = "/tmp/mode_" + id + ".json"
 file_mode = open(filename, "w")
 file_mode.write(json.dumps(mode))
 file_mode.close()
+filename = "/tmp/output_dir_" + id + ".json"
+file_output_dir = open(filename, "w")
+file_output_dir.write(json.dumps(output_dir))
+file_output_dir.close()
