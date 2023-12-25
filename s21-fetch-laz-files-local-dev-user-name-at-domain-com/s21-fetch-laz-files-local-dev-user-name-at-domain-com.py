c40c775043f45f0c7660d7d62c725c6857e97375
skoulouzis <skoulouzis@users.noreply.github.com>
2023-12-25 12:55:40 +0100

    s21-fetch-laz-files-local-dev-user-name-at-domain-com creation

---
+++ s21-fetch-laz-files-local-dev-user-name-at-domain-com/s21-fetch-laz-files-local-dev-user-name-at-domain-com.py
@@ -0,0 +1,35 @@
+import glob
+import os
+
+import argparse
+arg_parser = argparse.ArgumentParser()
+
+arg_parser.add_argument('--id', action='store', type=str, required=True, dest='id')
+
+
+arg_parser.add_argument('--local_path_split', action='store', type=str, required=True, dest='local_path_split')
+
+
+args = arg_parser.parse_args()
+print(args)
+
+id = args.id
+
+local_path_split = args.local_path_split.replace('"','')
+
+
+
+split_laz_folder = glob.glob(os.path.join(local_path_split, '*.LAZ'))
+split_laz_files = []
+print("File names ending with .LAZ:")
+for file_path in split_laz_folder:
+    split_laz_files.append(os.path.basename(file_path))
+
+print(split_laz_files)
+    
+
+import json
+filename = "/tmp/split_laz_files_" + id + ".json"
+file_split_laz_files = open(filename, "w")
+file_split_laz_files.write(json.dumps(split_laz_files))
+file_split_laz_files.close()
