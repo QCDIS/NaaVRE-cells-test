5d8559a1c60ec06ad65cec077a00dd6ef0bfa523
skoulouzis <skoulouzis@users.noreply.github.com>
2023-12-26 10:40:51 +0100

    s4-retrive-retiled-tiles-local-dev-user-name-at-domain-com update

--- s4-retrive-retiled-tiles-local-dev-user-name-at-domain-com/s4-retrive-retiled-tiles-local-dev-user-name-at-domain-com.py
+++ s4-retrive-retiled-tiles-local-dev-user-name-at-domain-com/s4-retrive-retiled-tiles-local-dev-user-name-at-domain-com.py
@@ -1,5 +1,6 @@
 import glob
 import os
+import pathlib
 
 import argparse
 arg_parser = argparse.ArgumentParser()
@@ -7,7 +8,7 @@ arg_parser = argparse.ArgumentParser()
 arg_parser.add_argument('--id', action='store', type=str, required=True, dest='id')
 
 
-arg_parser.add_argument('--local_path_retiled', action='store', type=str, required=True, dest='local_path_retiled')
+arg_parser.add_argument('--S3_done', action='store', type=str, required=True, dest='S3_done')
 
 
 args = arg_parser.parse_args()
@@ -15,28 +16,26 @@ print(args)
 
 id = args.id
 
-local_path_retiled = args.local_path_retiled.replace('"','')
+S3_done = args.S3_done.replace('"','')
 
 
+conf_local_path_retiled = os.path.join( pathlib.Path('/tmp/data').as_posix(), 'retiled')
 
+conf_local_path_retiled = os.path.join( pathlib.Path('/tmp/data').as_posix(), 'retiled')
+
+S3_done 
 
 tiles = []
-tile_folders = glob.glob(os.path.join(local_path_retiled, 'tile_*_*'))
+tile_folders = glob.glob(os.path.join(conf_local_path_retiled, 'tile_*_*'))
 
 for folder in tile_folders:
-    # Extract only the folder name without the 'local_path_retiled'
+    # Extract only the folder name without the 'conf_local_path_retiled'
     folder_name = os.path.basename(folder)
     tiles.append(folder_name)  # Append only the folder name
 print(tiles)
-
-path_retiled = local_path_retiled
 
 import json
 filename = "/tmp/tiles_" + id + ".json"
 file_tiles = open(filename, "w")
 file_tiles.write(json.dumps(tiles))
 file_tiles.close()
-filename = "/tmp/path_retiled_" + id + ".json"
-file_path_retiled = open(filename, "w")
-file_path_retiled.write(json.dumps(path_retiled))
-file_path_retiled.close()
