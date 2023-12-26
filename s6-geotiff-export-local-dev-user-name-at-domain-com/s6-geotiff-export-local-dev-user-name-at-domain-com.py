5a48a296873ddb7ed1ceb084c6f31ddb6dc8d593
skoulouzis <skoulouzis@users.noreply.github.com>
2023-12-26 10:41:19 +0100

    s6-geotiff-export-local-dev-user-name-at-domain-com update

--- s6-geotiff-export-local-dev-user-name-at-domain-com/s6-geotiff-export-local-dev-user-name-at-domain-com.py
+++ s6-geotiff-export-local-dev-user-name-at-domain-com/s6-geotiff-export-local-dev-user-name-at-domain-com.py
@@ -8,7 +8,7 @@ arg_parser = argparse.ArgumentParser()
 arg_parser.add_argument('--id', action='store', type=str, required=True, dest='id')
 
 
-arg_parser.add_argument('--local_path_targets', action='store', type=str, required=True, dest='local_path_targets')
+arg_parser.add_argument('--S5_done', action='store', type=str, required=True, dest='S5_done')
 
 arg_parser.add_argument('--param_hostname', action='store', type=str, required=True, dest='param_hostname')
 arg_parser.add_argument('--param_password', action='store', type=str, required=True, dest='param_password')
@@ -19,36 +19,37 @@ print(args)
 
 id = args.id
 
-local_path_targets = args.local_path_targets.replace('"','')
+S5_done = args.S5_done.replace('"','')
 
 param_hostname = args.param_hostname
 param_password = args.param_password
 param_username = args.param_username
 
-conf_local_tmp = pathlib.Path('/tmp/data')
-conf_remote_path_geotiffs = pathlib.Path('/webdav/vl-laserfarm/' +  'spiros.koulouzis@lifewatch.eu' + '/geotiffs')
+conf_local_path_targets = os.path.join( pathlib.Path('/tmp/data').as_posix(), 'targets')
+conf_local_path_geotiff = os.path.join( pathlib.Path('/tmp/data').as_posix(), 'geotiff')
+conf_remote_path_geotiffs = pathlib.Path('/webdav/vl-laserfarm/' +  '' + '/geotiffs')
 conf_feature_name = 'perc_95_normalized_height'
 conf_wd_opts = { 'webdav_hostname': param_hostname, 'webdav_login': param_username, 'webdav_password': param_password}
 
-conf_local_tmp = pathlib.Path('/tmp/data')
-conf_remote_path_geotiffs = pathlib.Path('/webdav/vl-laserfarm/' +  'spiros.koulouzis@lifewatch.eu' + '/geotiffs')
+conf_local_path_targets = os.path.join( pathlib.Path('/tmp/data').as_posix(), 'targets')
+conf_local_path_geotiff = os.path.join( pathlib.Path('/tmp/data').as_posix(), 'geotiff')
+conf_remote_path_geotiffs = pathlib.Path('/webdav/vl-laserfarm/' +  '' + '/geotiffs')
 conf_feature_name = 'perc_95_normalized_height'
 conf_wd_opts = { 'webdav_hostname': param_hostname, 'webdav_login': param_username, 'webdav_password': param_password}
 
-
-local_path_geotiff = os.path.join(conf_local_tmp.as_posix(), 'geotiff')
+S5_done
 
 geotiff_export_input = {
     'setup_local_fs': {
-        'input_folder': local_path_targets,
-         'output_folder': local_path_geotiff
+        'input_folder': conf_local_path_targets,
+         'output_folder': conf_local_path_geotiff
         },
     # 'pullremote': conf_remote_path_targets.as_posix(),
     'parse_point_cloud': {},
     'data_split': {'xSub': 1, 'ySub': 1},
     'create_subregion_geotiffs': {'output_handle': 'geotiff'},
     'pushremote': conf_remote_path_geotiffs.as_posix(),
-    'cleanlocalfs': {}   
+    # 'cleanlocalfs': {}   
 }
 
 writer = GeotiffWriter(input_dir=conf_feature_name, bands=conf_feature_name, label=conf_feature_name).config(geotiff_export_input).setup_webdav_client(conf_wd_opts)
