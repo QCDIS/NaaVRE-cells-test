6a87b4127d3c6a222ca6fe2ec1902f79c67dd6bc
skoulouzis <skoulouzis@users.noreply.github.com>
2023-12-28 17:32:56 +0100

    download-dev-user-name-at-domain-com update

--- download-dev-user-name-at-domain-com/download-dev-user-name-at-domain-com.py
+++ download-dev-user-name-at-domain-com/download-dev-user-name-at-domain-com.py
@@ -22,14 +22,18 @@ param_hostname = args.param_hostname
 param_password = args.param_password
 param_username = args.param_username
 
+conf_local_path_geotiff = os.path.join( pathlib.Path('/tmp/data').as_posix(), 'geotiff')
 conf_wd_opts = { 'webdav_hostname': param_hostname, 'webdav_login': param_username, 'webdav_password': param_password}
 conf_remote_path_geotiffs = pathlib.Path('/webdav/vl-laserfarm/' +  '' + '/geotiffs')
-conf_local_path_geotiff = os.path.join( pathlib.Path('/tmp/data').as_posix(), 'geotiff')
 
+conf_local_path_geotiff = os.path.join( pathlib.Path('/tmp/data').as_posix(), 'geotiff')
 conf_wd_opts = { 'webdav_hostname': param_hostname, 'webdav_login': param_username, 'webdav_password': param_password}
 conf_remote_path_geotiffs = pathlib.Path('/webdav/vl-laserfarm/' +  '' + '/geotiffs')
-conf_local_path_geotiff = os.path.join( pathlib.Path('/tmp/data').as_posix(), 'geotiff')
 
+
+print(conf_local_path_geotiff)
+if not os.path.exists(conf_local_path_geotiff):  # Check if folder doesn't exist
+    os.makedirs(conf_local_path_geotiff)  # Create the folder
 
 client = Client(conf_wd_opts)
 client.download_sync(remote_path=os.path.join(conf_remote_path_geotiffs,'geotiff_TILE_000_BAND_perc_95_normalized_height.tif'), local_path=os.path.join(conf_local_path_geotiff,'geotiff_TILE_000_BAND_perc_95_normalized_height.tif'))
