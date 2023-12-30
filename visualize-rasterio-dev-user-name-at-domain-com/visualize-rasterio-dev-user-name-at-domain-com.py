bbe86a6b9e7732def43c1ad6858e66666941ff28
skoulouzis <skoulouzis@users.noreply.github.com>
2023-12-30 17:51:29 +0100

    visualize-rasterio-dev-user-name-at-domain-com update

--- visualize-rasterio-dev-user-name-at-domain-com/visualize-rasterio-dev-user-name-at-domain-com.py
+++ visualize-rasterio-dev-user-name-at-domain-com/visualize-rasterio-dev-user-name-at-domain-com.py
@@ -21,11 +21,9 @@ id = args.id
 parameters = {}
 
 download_done = args.download_done.replace('"','')
-parameters['download_done'] = download_done
 
 
 conf_local_path_geotiff = os.path.join( pathlib.Path('/tmp/data').as_posix(), 'geotiff')
-parameters['conf_local_path_geotiff = os.path.join( pathlib.Path('/tmp/data').as_posix(), 'geotiff')'] = conf_local_path_geotiff = os.path.join( pathlib.Path('/tmp/data').as_posix(), 'geotiff')
 
 
 pm.execute_notebook(
