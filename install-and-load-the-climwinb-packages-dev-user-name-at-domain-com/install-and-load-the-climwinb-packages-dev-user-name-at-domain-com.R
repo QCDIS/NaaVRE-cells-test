1382db92fc8256011ff97dfa7a30216e46e28a95
skoulouzis <skoulouzis@users.noreply.github.com>
2024-01-16 20:54:15 +0100

    install-and-load-the-climwinb-packages-dev-user-name-at-domain-com update

--- install-and-load-the-climwinb-packages-dev-user-name-at-domain-com/install-and-load-the-climwinb-packages-dev-user-name-at-domain-com.R
+++ install-and-load-the-climwinb-packages-dev-user-name-at-domain-com/install-and-load-the-climwinb-packages-dev-user-name-at-domain-com.R
@@ -48,7 +48,6 @@ temperature_data_str <- toString(temperature_data)
 
 
 
-
 # capturing outputs
 file <- file(paste0('/tmp/rolling_mean_temp_str_', id, '.json'))
 writeLines(toJSON(rolling_mean_temp_str, auto_unbox=TRUE), file)
