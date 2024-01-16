1b5a6868eae1457d254475fa4d2564f6ab9c8d2d
skoulouzis <skoulouzis@users.noreply.github.com>
2024-01-16 20:50:48 +0100

    consume-climwin-dev-user-name-at-domain-com update

--- consume-climwin-dev-user-name-at-domain-com/consume-climwin-dev-user-name-at-domain-com.R
+++ consume-climwin-dev-user-name-at-domain-com/consume-climwin-dev-user-name-at-domain-com.R
@@ -9,8 +9,8 @@ library(jsonlite)
 option_list = list(
 
 make_option(c("--id"), action="store", default=NA, type="character", help="my description"), 
-make_option(c("--rolling_mean_temp"), action="store", default=NA, type="character", help="my description"), 
-make_option(c("--temperature_data"), action="store", default=NA, type="character", help="my description")
+make_option(c("--rolling_mean_temp_str"), action="store", default=NA, type="character", help="my description"), 
+make_option(c("--temperature_data_str"), action="store", default=NA, type="character", help="my description")
 
 )
 
@@ -19,16 +19,16 @@ opt = parse_args(OptionParser(option_list=option_list))
 
 
 id <- gsub('"', '', opt$id)
-rolling_mean_temp <- gsub('"', '', opt$rolling_mean_temp)
-temperature_data <- gsub('"', '', opt$temperature_data)
+rolling_mean_temp_str <- gsub('"', '', opt$rolling_mean_temp_str)
+temperature_data_str <- gsub('"', '', opt$temperature_data_str)
 
 
 
 
 
 
-cat("Original Temperature Data:\n", head(temperature_data), "\n\n")
-cat("Rolling Mean Temperature in Moving Windows:\n", head(coredata(rolling_mean_temp)), "\n")
+cat("Original Temperature Data:\n", head(temperature_data_str), "\n\n")
+cat("Rolling Mean Temperature in Moving Windows:\n", head(coredata(rolling_mean_temp_str)), "\n")
 
 
 
