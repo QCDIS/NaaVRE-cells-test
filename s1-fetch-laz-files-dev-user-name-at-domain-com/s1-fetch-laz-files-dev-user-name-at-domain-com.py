3ec3ff1ba75ae658450835dbc88c8b2466518150
skoulouzis <skoulouzis@users.noreply.github.com>
2024-01-23 14:24:30 +0100

    s1-fetch-laz-files-dev-user-name-at-domain-com update

--- s1-fetch-laz-files-dev-user-name-at-domain-com/s1-fetch-laz-files-dev-user-name-at-domain-com.py
+++ s1-fetch-laz-files-dev-user-name-at-domain-com/s1-fetch-laz-files-dev-user-name-at-domain-com.py
@@ -9,11 +9,8 @@ arg_parser = argparse.ArgumentParser()
 arg_parser.add_argument('--id', action='store', type=str, required=True, dest='id')
 
 
-arg_parser.add_argument('--param_bucket_name', action='store', type=str, required=True, dest='param_bucket_name')
 arg_parser.add_argument('--param_hostname', action='store', type=str, required=True, dest='param_hostname')
-arg_parser.add_argument('--param_minio_server', action='store', type=str, required=True, dest='param_minio_server')
 arg_parser.add_argument('--param_password', action='store', type=str, required=True, dest='param_password')
-arg_parser.add_argument('--param_remote_path_root', action='store', type=str, required=True, dest='param_remote_path_root')
 arg_parser.add_argument('--param_remote_server_type', action='store', type=str, required=True, dest='param_remote_server_type')
 arg_parser.add_argument('--param_username', action='store', type=str, required=True, dest='param_username')
 
@@ -23,30 +20,36 @@ print(args)
 id = args.id
 
 
-param_bucket_name = args.param_bucket_name
 param_hostname = args.param_hostname
-param_minio_server = args.param_minio_server
 param_password = args.param_password
-param_remote_path_root = args.param_remote_path_root
 param_remote_server_type = args.param_remote_server_type
 param_username = args.param_username
+
+conf_minio_server = parsed_url.hostname+':'+str(parsed_url.port)
+
+conf_bucket_name = path_parts[0]
+
+conf_remote_path_root = '/'+'/'.join(path_parts[1:])
 
 conf_wd_opts = { 'webdav_hostname': param_hostname, 'webdav_login': param_username, 'webdav_password': param_password}
 
 
+conf_minio_server = parsed_url.hostname+':'+str(parsed_url.port)
+conf_bucket_name = path_parts[0]
+conf_remote_path_root = '/'+'/'.join(path_parts[1:])
 conf_wd_opts = { 'webdav_hostname': param_hostname, 'webdav_login': param_username, 'webdav_password': param_password}
 
 
 laz_files = []
 if param_remote_server_type == 'minio':
-    minio_client = Minio(param_minio_server, secure=True)
-    objects = minio_client.list_objects(param_bucket_name, prefix=param_remote_path_root, recursive=True)
+    minio_client = Minio(conf_minio_server, secure=True)
+    objects = minio_client.list_objects(conf_bucket_name, prefix=conf_remote_path_root, recursive=True)
     for obj in objects:
         if obj.object_name.lower().endswith('.laz'):
             laz_files.append(obj.object_name.split('/')[-1])
 elif param_remote_server_type == 'webdav':
-    print(param_remote_path_root)
-    webdva_path = param_remote_path_root
+    print(conf_remote_path_root)
+    webdva_path = conf_remote_path_root
     laz_files = [f for f in list_remote(get_wdclient(conf_wd_opts), pathlib.Path(webdva_path).as_posix())
                  if f.lower().endswith('.laz')]
 print(laz_files)
